48244655c4b5cfb22679df95f96fdfb95f6cec8f
Bogdan Guranda <bogdan.guranda@ft.com>
2017-02-24 14:08:53 +0200

    Added test case, run go fmt.

--- relations/cypher_test.go
+++ relations/cypher_test.go
@@ -7,21 +7,30 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var givenNeoRelatedContent []neoRelatedContent
-var expectedRelatedContent []relatedContent
+var givenNeoRelatedContent []neoRelatedContent = []neoRelatedContent{
+	{UUID: "db90a9db-6cb6-4ba0-8648-c0676087aba2"},
+	{UUID: "f78c1482-abab-413e-b753-ca3ce3cb84f0"},
+}
 
-func TestTransformToRelatedContent(t *testing.T) {
+var expectedRelatedContent []relatedContent = []relatedContent{
+	{ID: "http://www.ft.com/thing/db90a9db-6cb6-4ba0-8648-c0676087aba2", APIURL: "http://api.ft.com/content/db90a9db-6cb6-4ba0-8648-c0676087aba2"},
+	{ID: "http://www.ft.com/thing/f78c1482-abab-413e-b753-ca3ce3cb84f0", APIURL: "http://api.ft.com/content/f78c1482-abab-413e-b753-ca3ce3cb84f0"},
+}
+
+func TestTransformToRelatedContentHappyFlow(t *testing.T) {
 	cd := NewCypherDriver(nil)
 
-	givenNeoRelatedContent = []neoRelatedContent{
-		{UUID: "db90a9db-6cb6-4ba0-8648-c0676087aba2"},
-		{UUID: "f78c1482-abab-413e-b753-ca3ce3cb84f0"},
-	}
+	relatedContent := cd.transformToRelatedContent(givenNeoRelatedContent)
 
-	expectedRelatedContent = []relatedContent{
-		{ID: "http://www.ft.com/thing/db90a9db-6cb6-4ba0-8648-c0676087aba2", APIURL: "http://api.ft.com/content/db90a9db-6cb6-4ba0-8648-c0676087aba2"},
-		{ID: "http://www.ft.com/thing/f78c1482-abab-413e-b753-ca3ce3cb84f0", APIURL: "http://api.ft.com/content/f78c1482-abab-413e-b753-ca3ce3cb84f0"},
-	}
+	expected, _ := json.Marshal(expectedRelatedContent)
+	actual, _ := json.Marshal(relatedContent)
+	assert.JSONEq(t, string(expected), string(actual))
+}
+
+func TestTransformToRelatedContentNoRelations(t *testing.T) {
+	cd := NewCypherDriver(nil)
+	givenNeoRelatedContent := []neoRelatedContent{}
+	expectedRelatedContent := []neoRelatedContent{}
 
 	relatedContent := cd.transformToRelatedContent(givenNeoRelatedContent)
 
